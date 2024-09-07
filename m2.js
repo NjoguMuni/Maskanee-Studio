737c715d1cc9c96a1acb2686d17941008dd9b6a9
Muni Ernest <120646939+MuniErnest@users.noreply.github.com>
2024-09-07 12:28:50 +0300

    first commit

---
+++ m2.js
@@ -0,0 +1,6 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    alert('Thank you for contacting Maskanee Studios!');
+    // Add additional JavaScript to handle form submission, such as sending an email using EmailJS or another service.
+});
